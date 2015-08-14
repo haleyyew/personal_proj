5144a88cb5c82a12bf3fc7d77a4051570913e016
Haoran <haleyyew@gmail.com>
2015-08-14 13:24:19 -0700

    Added Tkinter GUI

---
+++ Eulers_Method_Calculator/application_window_fn.py
@@ -0,0 +1,16 @@
+##Creating application window using functions
+
+#from Tkinter import *
+
+#root = Tk()
+#root.title("Euler's Method Calculator")
+#root.geometry("200x100")
+
+#app = Frame(root)
+#app.grid()
+#label = Label(app, text = "Enter your arguments:")
+#label.grid()
+
+#button = Button(app, text = "Get Solution")
+#button.grid()
+#root.mainloop()
